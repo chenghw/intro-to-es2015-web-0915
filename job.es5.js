9f3abbc5263d12699216fdc0d52787c528ffb964
chenghw <ho.won.cheng@gmail.com>
2015-11-19 13:41:45 -0500

    intro to es2015 complete

---
+++ job.es5.js
@@ -0,0 +1,18 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+// job.js
+
+var Job = function Job(title) {
+  _classCallCheck(this, Job);
+
+  this.title = title;
+};
+
+var validJobs = exports.validJobs = ["programmer", "that's it"];
+exports.default = Job;
---
+++ job.js
@@ -0,0 +1,9 @@
+// job.js
+class Job{
+  constructor(title){
+    this.title = title;
+  }
+}
+
+export var validJobs = ["programmer", "that's it"]
+export default Job;
